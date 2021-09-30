8046eab2a5346bfe21b8411fa2e20f57b5c9f751
Esteban Solano Granados <stvansolano@outlook.com>
2021-09-30 19:38:13 +0000

    Added swagger support to api

--- samples/dotnet-swagger/Program.cs
+++ samples/dotnet-swagger/Program.cs
@@ -31,37 +31,54 @@ namespace Dotnet_Backend
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {}
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddControllers();
+
+            // dotnet add package Swashbuckle.AspNetCore
+            services.AddSwaggerGen();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", context => {
-
-                    context.Response.Redirect("/api/hello");
-                    return Task.CompletedTask;
-                });
-
-                endpoints.MapGet("api/hello", async context => {
-                    
-                    await context.Response.WriteAsJsonAsync<object[]>(
-                        new object[]{
-                        "Hello",
-                        "World"
-                    });
-                });
+                endpoints.MapControllers();
             });
         }
+    }
+}
+
+namespace DotNet_Backend
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult SayHello() => base.Ok("Hello, World!".Split(" "));
     }
 }
