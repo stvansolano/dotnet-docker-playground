ec2e4617a33b5ec20741776fcbd55a4bd72272a4
Esteban Solano Granados <stvansolano@outlook.com>
2021-10-28 14:48:13 +0000

    Added dotnet-frontend sample

---
+++ samples/dotnet-frontend/Program.cs
@@ -0,0 +1,25 @@
+namespace DotNetFrontend
+{
+    using System;
+    using System.Net.Http;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { 
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
+
+            // dotnet run --project samples/frontend/Frontend.csproj --urls "http://*:80"
+            await builder.Build().RunAsync();
+        }
+    }
+}
